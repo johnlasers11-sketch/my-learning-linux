d954a602d58a0c126661949405e9223953e9a0b1
johnlasers11-sketch <johnlasers11@gmail.com>
2026-03-30 09:35:51 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-FOLDER="devops_test"
+# 1. Создаем имитацию большого лог-файла
+echo "2023-10-01 10:00:01 INFO: System started" > app.log
+echo "2023-10-01 10:05:23 WARNING: High memory usage" >> app.log
+echo "2023-10-01 10:10:45 ERROR: Connection lost to Database" >> app.log
+echo "2023-10-01 10:15:12 INFO: Retrying..." >> app.log
+echo "2023-10-01 10:20:00 ERROR: Authentication failed" >> app.log
 
-# ПЕРВЫЙ ПРОХОД
-echo "--- Шаг 1: Первая проверка ---"
-if [ ! -d "$FOLDER" ]; then
-  echo "Папки нет. Создаю..."
-  mkdir "$FOLDER"
-else
-  echo "Папка уже тут."
-fi
+echo "--- ВЕСЬ ФАЙЛ (для справки) ---"
+cat app.log
 
-# ВТОРОЙ ПРОХОД (в том же скрипте!)
-echo "--- Шаг 2: Вторая проверка (через 2 секунды) ---"
-sleep 2 # подождем чуть-чуть для солидности
+echo -e "\n--- ШАГ 1: Ищем только ошибки (ERROR) ---"
+grep "ERROR" app.log
 
-if [ ! -d "$FOLDER" ]; then
-  echo "Папки всё еще нет? Что-то пошло не так."
-else
-  echo "Ага! Теперь скрипт видит папку и НЕ будет её создавать заново."
-fi
+echo -e "\n--- ШАГ 2: Считаем, сколько раз была ошибка ---"
+grep -c "ERROR" app.log
 
-ls -ld "$FOLDER"
+echo -e "\n--- ШАГ 3: Ищем всё, КРОМЕ информационных сообщений (INFO) ---"
+grep -v "INFO" app.log
