e31020783fdd032bf7014acfc384d59fdd6476b5
johnlasers11-sketch <johnlasers11@gmail.com>
2026-03-30 09:38:22 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-# 1. Создаем имитацию большого лог-файла
-echo "2023-10-01 10:00:01 INFO: System started" > app.log
-echo "2023-10-01 10:05:23 WARNING: High memory usage" >> app.log
-echo "2023-10-01 10:10:45 ERROR: Connection lost to Database" >> app.log
-echo "2023-10-01 10:15:12 INFO: Retrying..." >> app.log
-echo "2023-10-01 10:20:00 ERROR: Authentication failed" >> app.log
+# Создаем наш лог снова (сделаем его чуть сложнее)
+echo "2023-10-01 10:00:01 INFO: System_started" > app.log
+echo "2023-10-01 10:05:23 WARNING: High_memory" >> app.log
+echo "2023-10-01 10:10:45 ERROR: Database_fail" >> app.log
 
-echo "--- ВЕСЬ ФАЙЛ (для справки) ---"
+echo "--- ВЕСЬ ЛОГ ---"
 cat app.log
 
-echo -e "\n--- ШАГ 1: Ищем только ошибки (ERROR) ---"
-grep "ERROR" app.log
+echo -e "\n--- ШАГ 1: Берем только ВРЕМЯ (2-я колонка) ---"
+awk '{print $2}' app.log
 
-echo -e "\n--- ШАГ 2: Считаем, сколько раз была ошибка ---"
-grep -c "ERROR" app.log
+echo -e "\n--- ШАГ 2: Берем ТИП события (3-я колонка) ---"
+awk '{print $3}' app.log
 
-echo -e "\n--- ШАГ 3: Ищем всё, КРОМЕ информационных сообщений (INFO) ---"
-grep -v "INFO" app.log
+echo -e "\n--- ШАГ 3: Комбинируем (Время и Тип) ---"
+awk '{print "Событие [" $3 "] произошло в " $2}' app.log
+
