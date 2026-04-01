9dfe3fded205077e9f8b92529fd6037e32229307
johnlasers11-sketch <johnlasers11@gmail.com>
2026-04-01 10:37:41 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# 1. Создаем "конфиг" файл
 echo "server_address: localhost" > config.yaml
 echo "port: 8080" >> config.yaml
 echo "status: debug" >> config.yaml
@@ -8,11 +7,8 @@ echo "status: debug" >> config.yaml
 echo "--- ФАЙЛ ДО ЗАМЕНЫ ---"
 cat config.yaml
 
-# 2. Магия SED: меняем localhost на реальный адрес
-# s - значит substitute (заменить), /что/на что/
 sed -i 's/localhost/://production-server.com' config.yaml
 
-# 3. Еще одна замена: меняем режим с debug на active
 sed -i 's/debug/active/' config.yaml
 
 echo -e "\n--- ФАЙЛ ПОСЛЕ ЗАМЕНЫ ---"
